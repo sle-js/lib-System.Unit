3b077e7479439b8f6b6ab15f55ce3a6b7b1d4c38
Graeme Lockley <graemel@no9.co.za>
2017-07-09 08:04:23 +0200

    refactor: remove use of isAllGood

--- test/UnitTest.js
+++ test/UnitTest.js
@@ -26,15 +26,19 @@ const tests = Unit.Suite("Test Suite")([
 const totalTests = unitTest =>
     unitTest.reduce(name => tests => Array.foldl(0)(acc => t => acc + totalTests(t))(tests))(name => assertion => 1);
 
-const isAllGood = (assertion) =>
-    assertion.reduce(x => true)(file => lineno => msg => false);
 
-const passedTests = unitTest =>
-    unitTest.reduce(name => tests => Array.foldl(0)(acc => t => acc + passedTests(t))(tests))(name => assertion => isAllGood(assertion) ? 1 : 0);
+const passedTests = unitTest => {
+    const countPassedTests = count => unitTest =>
+        unitTest.reduce(
+            name => tests => Array.foldl(count)(acc => t => countPassedTests(acc)(t))(tests)
+        )(name => assertion =>
+            assertion.then(_ => count.then(c => Promise.resolve(c + 1))).catch(_ => count));
+
+    return countPassedTests(Promise.resolve(0))(unitTest);
+};
 
 
 module.exports = Unit.Suite("Unit Test Test")([
     Unit.Test("Total tests is 4")(Unit.AllGood.equals(4)(totalTests(tests))),
-    Unit.Test("Passed tests is 2")(Unit.AllGood.equals(2)(passedTests(tests)))
+    Unit.Test("Passed tests is 2")(passedTests(tests).then(count => Unit.AllGood.equals(2)(count)))
 ]);
-
